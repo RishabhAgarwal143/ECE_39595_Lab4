eb17b09748af43f468e366759d59cf5d367ea9a7
Mahesh Madhavan <mmadhava@eceprog6.ecn.purdue.edu>
2023-11-16 16:36:15 -0500

    fixed add 2

--- poly.cpp
+++ poly.cpp
@@ -90,12 +90,13 @@ polynomial polynomial::operator+(const int other) const{
         new_poly.push_back(std::make_pair(it->first, it->second));
     }
     auto back = this->poly.back();
-    if (back.first == 0){
+    if (back.first == 0) {
+        new_poly.pop_back();
+        new_poly.push_back(std::make_pair(0, back.second + other));
+    } else {
         new_poly.push_back(std::make_pair(0, other));
     }
-    else{
-        new_poly.push_back(std::make_pair(0, back.second + other));
-    }
+
     return polynomial(new_poly.begin(), new_poly.end());
 }
 
